6ec88cc008c7e52309a37a336393f01b2c8b9943
Sorin Mihai Broscaru <broscaru.sorin@gmail.com>
2023-10-13 13:33:35 +0300

    Add cv meta and optimisations (#2)
    * Added page meta info
    
    * Update language matrix to allow for accesability

--- src/lib/components/cv/contents/language-skills/LanguageMatrix.svelte
+++ src/lib/components/cv/contents/language-skills/LanguageMatrix.svelte
@@ -1,19 +1,23 @@
 <table>
-	<tr>
-		<th colspan="2" />
-		<th colspan="2">Understanding</th>
-		<th colspan="2">Speaking</th>
-		<th colspan="2">Writing</th>
-	</tr>
-	<tr class="subheader">
-		<th colspan="2" />
-		<th>Listening</th>
-		<th>Reading</th>
-		<th>Spoken Production</th>
-		<th>Spoken Interaction</th>
-		<th colspan="2" />
-	</tr>
-	<slot />
+	<thead>
+		<tr>
+			<th colspan="2">Language</th>
+			<th colspan="2">Understanding</th>
+			<th colspan="2">Speaking</th>
+			<th colspan="2">Writing</th>
+		</tr>
+		<tr class="subheader">
+			<th colspan="2" />
+			<th>Listening</th>
+			<th>Reading</th>
+			<th>Spoken Production</th>
+			<th>Spoken Interaction</th>
+			<th colspan="2" />
+		</tr>
+	</thead>
+	<tbody>
+		<slot />
+	</tbody>
 	<caption
 		>Levels: A1 and A2: Basic user; B1 and B2: Independent user; C1 and C2: Proficient user</caption
 	>
@@ -45,6 +49,6 @@
 		font-weight: 400;
 		font-style: italic;
 		padding: 0.2em;
-		opacity: 0.5;
+		opacity: 0.7;
 	}
 </style>
