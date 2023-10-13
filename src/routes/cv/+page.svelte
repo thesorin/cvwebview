87e1c4bcd1d68cfe89de4ef4dfbfcbeb73500e35
Broscaru Sorin Mihai <broscaru.sorin@gmail.com>
2023-10-13 13:14:29 +0300

    Added page meta info

--- src/routes/cv/+page.svelte
+++ src/routes/cv/+page.svelte
@@ -12,6 +12,11 @@
 <svelte:head>
 	<title>Broscaru Sorin Mihai Curriculum Vitae</title>
 	<link rel="icon" href={favicon} />
+	<meta charset="UTF-8" />
+	<meta name="keywords" content="CV, Sorin Mihai Broscaru, Broscaru, Sorin Broscaru" />
+	<meta name="description" content="Sorin Mihai Broscaru Curriculum Vitae" />
+	<meta name="author" content="Sorin Mihai Broscaru" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 </svelte:head>
 
 <style>
