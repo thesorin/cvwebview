de3e27639fd9c1fd82df1e1a95bc5c0bc03b3bc8
Broscaru Sorin Mihai <broscaru.sorin@gmail.com>
2023-10-15 15:06:14 +0300

    Add introduction and finish up section

--- src/lib/components/cv/contents/Contents.svelte
+++ src/lib/components/cv/contents/Contents.svelte
@@ -2,11 +2,13 @@
 	import AdditionalInformation from './additional-information/AdditionalInformation.svelte';
 	import DigitalSkills from './digital-skills/DigitalSkills.svelte';
 	import EducationAndTraining from './education-and-training/EducationAndTraining.svelte';
+	import Introduction from './introduction/Introduction.svelte';
 	import LanguageSkills from './language-skills/LanguageSkills.svelte';
 	import WorkExperience from './work-experience/WorkExperience.svelte';
 </script>
 
 <main>
+	<Introduction />
 	<WorkExperience />
 	<EducationAndTraining />
 	<LanguageSkills />
