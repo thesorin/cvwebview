a4c731f3ff7dea75542c28927be79e1c485587a9
Broscaru Sorin Mihai <broscaru.sorin@gmail.com>
2023-10-24 14:32:37 +0300

    Fix airtame phrasing

--- src/lib/components/cv/contents/work-experience/Airtame.svelte
+++ src/lib/components/cv/contents/work-experience/Airtame.svelte
@@ -11,30 +11,36 @@
 	startDate={new Date(2022, 4, 16)}
 	location="Remote, Denmark"
 >
-	<SectionParagraph
-		>At Airtame, I have two projects that I work on. Initially I was helping out the team on the
-		main conferencing project, written in React 18+ with Typescript and scss. The project itself was
-		composed from 2 web applications, one running in Electron and the other one running on the
-		device designed by Airtame. Over time, while business has grown, a new project was setup by the
-		team and I inherited the role of taking over as main mainteiner and devleoper on it, a
-		application wirtten for the browser to be used along with Airtame devices.
+	<SectionParagraph>
+		At Airtame, I have two projects that I work on. Initially, I was helping out the team on the
+		main conferencing project, written in React 18+ with Typescript and SCSS. The project itself was
+		composed of several web applications, but it was mainly split into two logical parts, one
+		running in Electron as an interface for the user to control the device, and the other one
+		running on the device designed by Airtame. Over time, with the business growing, a new project
+		that runs strictly in the browser and it was created so that, in time, it would take over the
+		application that runs in Electron. For this newer project, I took over as the main maintainer
+		and further develop functionality.
 	</SectionParagraph>
 	<SectionParagraph>
-		The core responsabilies I have at Airtame are:
+		The core responsibilities I have at Airtame are:
 		<SectionUl>
 			<SectionLi>Develop and implement software features from UX team</SectionLi>
-			<SectionLi>Maintain, update and contribute to main codebase of the two projects</SectionLi>
+			<SectionLi>
+				Maintain, update, and contribute to the main codebase of the two projects
+			</SectionLi>
 			<SectionLi>Write unit test and storybook scenarios.</SectionLi>
-			<SectionLi>Fix bugs present in existing codebase as well as new discovered ones.</SectionLi>
-			<SectionLi>Contribute to company's common components library.</SectionLi>
+			<SectionLi>
+				Fix bugs present in the existing codebase as well as newly discovered ones.
+			</SectionLi>
+			<SectionLi>Contribute to the company's common components library.</SectionLi>
 			<SectionLi>Participate in hiriing interviews.</SectionLi>
 		</SectionUl>
 	</SectionParagraph>
 	<SectionParagraph>
-		And the techonologies used:
+		And the technologies used:
 		<SectionUl>
 			<SectionLi>Frontend framework: React 18+ with Typescript and SCSS.</SectionLi>
-			<SectionLi>Tools used: Vite, esbuild, webpack, Storybook 6</SectionLi>
+			<SectionLi>Tools used: Vite, Esbuild, Webpack, Storybook 6</SectionLi>
 			<SectionLi>Testing framework: Vitest, Jest.</SectionLi>
 		</SectionUl>
 	</SectionParagraph>
